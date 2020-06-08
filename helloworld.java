7b6e0690b7025b8e74ab9790bb2a536c54b2006b
dhkdevops <dhk12345@yahoo.com>
2020-06-08 13:01:18 +0000

    new program added

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,8 @@
 class Simple{
 public static void main(String args[]){
 System. out. println("Hello Java");
+System. out. println("Hello Java");
+
 System. out. println("Git tutorial");
 System. out. printn("testing");
 }
